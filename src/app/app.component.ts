27c25ea83c00282e21a7b2e6af19a91c7a844f78
Julian Lasso <julian.lasso@sofka.com.co>
2022-06-24 10:51:47 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sofka-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'appSofkaUniversity';
+}
